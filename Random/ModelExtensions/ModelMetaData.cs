36ee1f545f9b0e82a09c369e81e6c6fe88fb3b51
Niño Angelo Ty <ninoangeloty@yahoo.com>
2015-06-04 15:42:24 +0800

    model meta data

---
+++ Random/ModelExtensions/ModelMetaData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomJunk.ModelExtensions
+{
+    public static class ModelMetaData
+    {
+        public static void GetModelMetaData<T>(Expression<Func<T, object>> expression)
+        {
+            string propertyName = null;
+            Type containerType = null;
+
+            if (expression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                MemberExpression memberExpression = (MemberExpression)expression.Body;
+                propertyName = memberExpression.Member is PropertyInfo ? memberExpression.Member.Name : null;
+                containerType = memberExpression.Expression.Type;
+            }
+        }
+    }
+}
